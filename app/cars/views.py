475d148c6ecaab71ff00b25bb81bd3b3adf869ec
Przemysław Gawłowski <gawlowski.przemyslaw@protonmail.com>
2021-01-28 18:48:05 +0100

    Create Car view set

--- app/cars/views.py
+++ app/cars/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from core.models import Car
+
+from cars import serializers
+
+
+class CarViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """Manage cars in the database"""
+    queryset = Car.objects.all()
+    serializer_class = serializers.CarSerializer
+
+    def get_queryset(self):
+        return self.queryset.order_by('-make')
