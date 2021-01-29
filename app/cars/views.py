2b1597e8765accc03792525617a0879521be1cb3
Przemysław Gawłowski <gawlowski.przemyslaw@protonmail.com>
2021-01-29 10:58:05 +0100

    Create new car

--- app/cars/views.py
+++ app/cars/views.py
@@ -5,10 +5,15 @@ from core.models import Car
 from cars import serializers
 
 
-class CarViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class CarViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
     """Manage cars in the database"""
     queryset = Car.objects.all()
     serializer_class = serializers.CarSerializer
 
     def get_queryset(self):
-        return self.queryset.order_by('-make')
+        """Get all cars"""
+        return self.queryset.order_by('-id')
+
+    def perform_create(self, serializer):
+        """Create new car"""
+        serializer.save()
