bd8a33b7d189346ce5fd403f46f5b394477bcb88
Przemysław Gawłowski <gawlowski.przemyslaw@protonmail.com>
2021-01-29 11:34:13 +0100

    Create new car

--- app/cars/admin.py
+++ app/cars/admin.py
@@ -7,5 +7,8 @@ class CarAdmin(admin.ModelAdmin):
     ordering = ['id']
     list_display = ['make', 'model', 'created_at', 'updated_at']
 
+    def save_model(self, request, obj, form, change):
+        super(CarAdmin, self).save_model(request, obj, form, change)
+
 
 admin.site.register(Car, CarAdmin)
