9917f0fec2d1b80f3a8dc6a2b535c13d4082920f
Przemysław Gawłowski <gawlowski.przemyslaw@protonmail.com>
2021-01-29 15:25:14 +0100

    Check that the make and model are valid

--- app/cars/serializers.py
+++ app/cars/serializers.py
@@ -13,14 +13,19 @@ class CarSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_Fields = ('id',)
 
-    def validate_make(self, value):
+    def validate(self, attrs):
         """
-        Check that the make is valid
+        Check that the make and model are valid
         """
-        response = requests.get(f"https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/{value.upper()}?format=json")
+        response = requests.get(f"https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/{attrs.get('make').upper()}?format=json")
         results = response.json().get('Results')
 
         if not results:
-            raise serializers.ValidationError("Car make does not exists")
+            raise serializers.ValidationError("Car make is invalid")
 
-        return value
+        model = [car for car in results if car['Model_Name'] == attrs.get('model')]
+
+        if not model:
+            raise serializers.ValidationError("Car model is invalid")
+
+        return attrs
