77bb4536f2be81c7754f497087d4f64e4812dd87
Przemysław Gawłowski <gawlowski.przemyslaw@protonmail.com>
2021-01-29 15:18:09 +0100

    Validate make

--- app/cars/serializers.py
+++ app/cars/serializers.py
@@ -1,3 +1,5 @@
+import requests
+import logging
 from rest_framework import serializers
 
 from core.models import Car
@@ -8,5 +10,17 @@ class CarSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Car
-        fields = ('id', 'make', 'model', 'created_at', 'updated_at')
+        fields = '__all__'
         read_only_Fields = ('id',)
+
+    def validate_make(self, value):
+        """
+        Check that the make is valid
+        """
+        response = requests.get(f"https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/{value.upper()}?format=json")
+        results = response.json().get('Results')
+
+        if not results:
+            raise serializers.ValidationError("Car make does not exists")
+
+        return value
