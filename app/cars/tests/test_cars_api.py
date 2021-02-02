4c8e67f30b0ea19a4c71d1db3f02142f1ddfb026
Przemysław Gawłowski <gawlowski.przemyslaw@protonmail.com>
2021-02-02 11:53:48 +0100

    Test for endpoint to create new car

--- app/cars/tests/test_cars_api.py
+++ app/cars/tests/test_cars_api.py
@@ -19,9 +19,22 @@ class PublicCarsApiTests(TestCase):
         self.client = APIClient()
 
     def test_retrieve_cars(self):
+        """Test retrieving list of cars"""
         res = self.client.get(CARS_URL)
 
         cars = Car.objects.all().order_by("id")
         serializer = CarSerializer(cars, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
+
+    def test_create_car_successfully(self):
+        """Test creating a new car"""
+        payload = {'make': 'Honda', 'model': 'Accord'}
+        res = self.client.post(CARS_URL, payload)
+
+        exists = Car.objects.filter(
+            make=payload['make'],
+            model=payload['model']
+        ).exists()
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(exists)
